d6441166f03aef3000a97507a05ba04088e5d298
Keith Maxwell <keith.maxwell@gmail.com>
2025-01-08 01:45:31 +0000

    chore: increment version number to 0.0.13

--- src/dotlocalslashbin.py
+++ src/dotlocalslashbin.py
@@ -28,7 +28,7 @@ from urllib.request import urlopen
 from zipfile import ZipFile
 
 
-__version__ = "0.0.12"
+__version__ = "0.0.13"
 
 _HOME = str(Path("~").expanduser())
 _OUTPUT = Path("~/.local/bin/")
