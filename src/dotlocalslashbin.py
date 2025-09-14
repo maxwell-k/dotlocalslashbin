cb4570426901ca2d8c6ae59eb8df8122b6892436
Keith Maxwell <keith.maxwell@gmail.com>
2025-09-14 23:04:33 +0100

    chore: increment version number to 0.0.20

--- src/dotlocalslashbin.py
+++ src/dotlocalslashbin.py
@@ -23,7 +23,7 @@ from urllib.request import urlopen
 from zipfile import ZipFile
 
 
-__version__ = "0.0.19"
+__version__ = "0.0.20"
 
 _CACHE = Path("~/.cache/dotlocalslashbin/")
 _HOME = str(Path("~").expanduser())
